f5d75b4f714b73cc9d18f227f497d45204eafca8
Abhigyan001 <abhigyanmahanta001@gmail.com>
2022-12-03 15:48:04 -0500

    Configure postgresql db and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+70.times do
+    Article.create(
+    title: Faker::Game.title,
+    body: Faker::Lorem.paragraph(sentence_count: 15)
+    )
+  end
