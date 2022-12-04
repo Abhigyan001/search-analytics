37c4fcdf0245cd9fbff813e6dde702ef1f520e86
Abhigyan001 <abhigyanmahanta001@gmail.com>
2022-12-04 01:22:57 -0500

    add article display page and refactor search logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,30 +10,30 @@ class ArticlesController < ApplicationController
   end
 
   def search
-    @articles = Article.where('title LIKE ?', "%#{params[:query]}%").first(10)
-    render '/articles/index', locals: { articles: @articles }
-    query_save(params[:query], session[:user_id])
+    if params[:query].empty?
+      render '/articles/index', locals: { articles: [] }
+    else
+      @articles = Article.search_article(params[:query])
+      render '/articles/index', locals: { articles: @articles }
+      query_save(params[:query], session[:user_id], @articles.length)
+    end
   end
 
   protected
 
   # save new query
 
-  def query_save(query, session)
+  def query_save(query, session, articles)
     return if query.length <= 2 || query.nil?
 
-    new_query = Analytic.new(user_params)
+    new_query = Analytic.new(query: query, articles: articles)
     new_query.session_id = session
     session_last_query = Analytic.where(session_id: session).last
     if session_last_query.nil? || !session_last_query.searched_query(query)
       new_query.save
     elsif session_last_query.query.length < query.length
-      session_last_query.update(query: query)
+      session_last_query.update(query: query, articles: articles)
     end
-  end
-
-  def user_params
-    params.permit(:query)
   end
 
   def session_present
