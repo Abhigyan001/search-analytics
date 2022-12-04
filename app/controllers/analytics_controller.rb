cfe5bdcc985f19a14bcabbdf4e56257678a24b56
Abhigyan001 <abhigyanmahanta001@gmail.com>
2022-12-04 00:30:25 -0500

    display last searches in reverse order

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,2 +1,13 @@
 class AnalyticsController < ApplicationController
+  def query_list
+    Analytic.group(:query).pluck('query, count(query) as COUNT')
+  end
+
+  def sort_query_list
+    query_list.sort_by { |element| element[1] }.reverse.first(50)
+  end
+
+  def index
+    @user_search_list = sort_query_list
+  end
 end
