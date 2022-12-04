8dbb5fbce51272793dac3eb9b4f2c6925518fbb2
Abhigyan001 <abhigyanmahanta001@gmail.com>
2022-12-04 09:40:58 -0500

    Add specs for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 7, maximum: 60 }
-  validates :body, presence: true, length: { minimum: 18, maximum: 300 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 20, maximum: 300 }
 
   def self.search_article(query)
     where('title LIKE ?', "%#{query}%").pluck(:title, :id).first(10) if exists?
