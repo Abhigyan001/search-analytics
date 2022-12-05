6e2f694f2bbd76a0ad77918ea170506dbdeb6b6f
Abhigyan001 <abhigyanmahanta001@gmail.com>
2022-12-05 04:09:38 -0500

    add redirect specs

--- spec/controllers/analytics_controller_spec.rb
+++ spec/controllers/analytics_controller_spec.rb
@@ -3,31 +3,39 @@ require 'rails_helper'
 describe 'Analytics Controller', type: :feature do
   session_id = SecureRandom.urlsafe_base64(12)
 
-  before(:all) do
-    30.times do |e|
+  context 'Analytics Table' do
+    before(:each) do
+      30.times do |e|
+        Analytic.create(
+          query: "query#{e}",
+          session_id: session_id
+        )
+      end
+    end
+
+    it 'should display analytics page' do
+      visit '/analytics'
+      expect(page).to have_content('Search Frequency')
+    end
+
+    it 'should only show top 15 queries' do
+      visit '/analytics'
+      expect(page).to have_content('query9')
+    end
+
+    it 'should not show queries that are not in top 15' do
       Analytic.create(
-        query: "query#{e}",
+        query: 'above fifteen',
         session_id: session_id
       )
+      visit '/analytics'
+      expect(page).to_not have_content('above fifteen')
     end
-  end
 
-  it 'should display analytics page' do
-    visit '/analytics'
-    expect(page).to have_content('Search Frequency')
-  end
-
-  it 'should only show top 15 queries' do
-    visit '/analytics'
-    expect(page).to have_content('query9')
-  end
-
-  it 'should not show queries that are not in top 15' do
-    Analytic.create(
-      query: 'above fifteen',
-      session_id: session_id
-    )
-    visit '/analytics'
-    expect(page).to_not have_content('above fifteen')
+    it 'should redirect to homepage' do
+      visit '/analytics'
+      find('a', text: 'Go to Homepage').click
+      expect(page).to have_css('#search-form')
+    end
   end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 describe 'Articles Controller', type: :feature do
   it 'should have a search-bar' do
     visit '/'
-    expect(page).to have_css('.search-form')
+    expect(page).to have_css('#search-form')
   end
 
   it 'should have a body' do
     visit '/'
     expect(page).to have_css('.heading-img')
+  end
+
+  it 'should redirect to search analytics page' do
+    visit '/'
+    find('a', text: 'See Search Analytics').click
+    expect(page).to have_content('Search Frequency')
   end
 end
